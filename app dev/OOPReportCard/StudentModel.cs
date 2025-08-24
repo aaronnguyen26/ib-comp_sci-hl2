ae1e216da5cc6f337fa95579a90f47b8a1f79656
Aaron Nguyen <minh7898888@gmail.com>
2025-08-23 17:29:27 -0700

    commit

--- app dev/OOPReportCard/StudentModel.cs
+++ app dev/OOPReportCard/StudentModel.cs
@@ -1,4 +1,16 @@
 public class StudentModel : PersonModel
 {
     public int GradeLevel { get; set; }
+    public int DistrictStudentID { get; set; }
+
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        string output;
+        output = $"{FirstName}, {LastName}\nGrade: {GradeLevel}, Age: {Age}";
+        return output;
+    }
+
+
 }
